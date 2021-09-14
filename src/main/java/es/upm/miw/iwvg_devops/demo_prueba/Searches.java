5c482612c4f47d2324dd77329e191aa090a6674d
Mariavernia <maversa2@alumni.uv.es>
2021-09-14 11:45:27 +0200

    Add all code to the repo

---
+++ src/main/java/es/upm/miw/iwvg_devops/demo_prueba/Searches.java
@@ -0,0 +1,109 @@
+package es.upm.miw.iwvg_devops.demo_prueba;
+
+import org.apache.logging.log4j.LogManager;
+
+import java.util.stream.Stream;
+
+public class Searches {
+    public Stream<String> findUserFamilyNameByUserNameDistinct(String userName) {
+        return new UsersDatabase().findAll()
+                .filter(user -> userName.equals(user.getName()))
+                .map(User::getFamilyName)
+                .distinct();
+    }
+
+    public Stream<Integer> findFractionNumeratorByUserFamilyName(String userFamilyName) {
+        return new UsersDatabase().findAll()
+                .peek(x -> LogManager.getLogger(this.getClass()).info("before: " + x))
+                .filter(user -> userFamilyName.equals(user.getFamilyName()))
+                .peek(x -> LogManager.getLogger(this.getClass()).info("after: " + x))
+                .flatMap(user -> user.getFractions().stream())
+                .map(Fraction::getNumerator);
+    }
+
+    public Stream<String> findUserFamilyNameByFractionDenominator(int fractionDenominator) {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(fraction -> fractionDenominator == fraction.getDenominator()))
+                .map(User::getFamilyName);
+    }
+
+    public Stream<String> findUserFamilyNameInitialByAnyProperFraction() {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(fraction -> fraction.getDenominator() > fraction.getNumerator()))
+                .map(User::initialsFamilyName);
+    }
+
+    public Stream<String> findUserIdByAnyProperFraction() {
+        return new UsersDatabase().findAll()
+                .filter(user -> user.getFractions().stream()
+                        .anyMatch(fraction -> fraction.getDenominator() > fraction.getNumerator()))
+                .map(User::getId);
+    }
+
+    /*public Fraction findFractionMultiplicationByUserFamilyName(String familyName) {
+        return new UsersDatabase().findAll()
+                .peek(x -> LogManager.getLogger(this.getClass()).info("before: " + x))
+                .filter(user -> familyName.equals(user.getFamilyName()))
+                .peek(x -> LogManager.getLogger(this.getClass()).info("after: " + x))
+                .flatMap(user -> user.getFractions().stream())
+                .map(Fraction::getMultiplication);
+    }
+
+    public Fraction findFirstFractionDivisionByUserId(String id) {
+        return new UsersDatabase().findAll()
+                .filter(fraction -> id.equals(fraction.getFractions().stream().findFirst()))
+                .map(Fraction::decimal);
+    }
+
+    public Double findFirstDecimalFractionByUserName(String name) {
+        return new UsersDatabase().findAll()
+                .filter(fraction -> name.equals(fraction.getFractions().stream().findFirst()))
+                .map(Fraction::decimal);
+    }*/
+
+    public Stream<String> findUserIdByAllProperFraction() {
+        return Stream.empty();
+    }
+
+    public Stream<Double> findDecimalImproperFractionByUserName(String name) {
+        return Stream.empty();
+    }
+
+    public Fraction findFirstProperFractionByUserId(String id) {
+        return null;
+    }
+
+    public Stream<String> findUserFamilyNameByImproperFraction() {
+        return Stream.empty();
+    }
+
+    public Fraction findHighestFraction() {
+        return null;
+    }
+
+    public Stream<String> findUserNameByAnyImproperFraction() {
+        return Stream.empty();
+    }
+
+    public Stream<String> findUserFamilyNameByAllNegativeSignFractionDistinct() {
+        return Stream.empty();
+    }
+
+    public Stream<Double> findDecimalFractionByUserName(String name) {
+        return Stream.empty();
+    }
+
+    public Stream<Double> findDecimalFractionByNegativeSignFraction() {
+        return Stream.empty();
+    }
+
+    public Fraction findFractionAdditionByUserId(String id) {
+        return null;
+    }
+
+    public Fraction findFirstFractionSubtractionByUserName(String name) {
+        return null;
+    }
+}
